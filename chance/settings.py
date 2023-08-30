bc491bcc07d502f3b200b380299ae76e32a4ea43
yasreb <yasribseemab@gmail.com>
2023-08-30 06:28:44 -0700

    this is the last project of python course

--- chance/settings.py
+++ chance/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-d#g(*+l)6rzza55!wjuj&7vt21!wlv*050apgty#8fm$u=w)yp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
